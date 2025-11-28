5c92f7e60903846569c0d685a1832406ebcca4b3
PavloTytko <tytkotytko1998@gmail.com>
2025-11-28 12:34:55 +0200

    products page update

--- app/src/pages/products/index.tsx
+++ app/src/pages/products/index.tsx
@@ -1,50 +1,48 @@
-import {GetServerSideProps} from "next";
-import React, {useEffect, useState} from "react";
-import {fetchOrders, Order, removeOrder, setOrders} from "../../store/slices/ordersSlice";
+import { GetServerSideProps } from "next";
+import React, { useEffect } from "react";
 import axios from "axios";
-import {useDispatch, useSelector} from "react-redux";
-import {RootState} from "../../store/store";
-import OrdersList from "../../components/OrdersList/OrdersList";
-import OrderSidebar from "../../components/OrderSidebar/OrderSidebar";
-import DeleteModal from "../../components/DeleteModal/DeleteModal";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import ProductsList from "../../components/ProductsList/ProductsList";
 import withAuth from "../../components/protectedRoute/withAuth";
+import {
+  fetchProducts,
+  Product,
+  setProducts,
+} from "../../store/slices/productsSlice";
 
-const OrdersPage: React.FC<{ initialOrders?: Order[] }> = ({initialOrders}) => {
-    const dispatch = useDispatch();
-    const {items, loading} = useSelector((s: RootState) => s.orders);
-    const [selected, setSelected] = useState<Order | null>(null);
-    const [showDelete, setShowDelete] = useState<number | null>(null);
+const ProductsPage: React.FC<{ initialProducts?: Product[] }> = ({
+  initialProducts,
+}) => {
+  const dispatch = useDispatch();
+  const { items, loading } = useSelector((s: RootState) => s.products);
 
-    useEffect(() => {
-        dispatch(setOrders(initialOrders || []));
-        dispatch(fetchOrders() as any);
-    }, [dispatch, initialOrders]);
+  useEffect(() => {
+    // hydrate from SSR then ensure fresh data on client
+    dispatch(setProducts(initialProducts || []));
+    dispatch(fetchProducts() as any);
+  }, [dispatch, initialProducts]);
 
-    const confirmDelete = (id: number) => {
-        // local deletion - ideally call API delete
-        dispatch(removeOrder(id));
-        setShowDelete(null);
-        if (selected?.id === id) setSelected(null);
-    };
+  return (
+    <div style={{ width: "100%" }}>
+      {loading && items.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <ProductsList products={items} />
+      )}
+    </div>
+  );
+};
 
-    return (
-        <div style={{display: "flex", gap: 12}}>
-            <OrdersList orders={items} onSelect={(o) => setSelected(o)} onDelete={(id) => setShowDelete(id)}
-                        loading={loading}/>
-            <OrderSidebar order={selected} onClose={() => setSelected(null)}/>
-            {showDelete &&
-                <DeleteModal onClose={() => setShowDelete(null)} onConfirm={() => confirmDelete(showDelete)}/>}
-        </div>
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL || "http://api:4000"}/rest/products`,
     );
+    return { props: { initialProducts: res.data } };
+  } catch (e) {
+    return { props: { initialProducts: [] } };
+  }
 };
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    try {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL || "http://api:4000"}/rest/orders`);
-        return {props: {initialOrders: res.data}};
-    } catch (e) {
-        return {props: {initialOrders: []}};
-    }
-};
-
-export default withAuth(OrdersPage);
+export default withAuth(ProductsPage);
